96a9c704a61cec5d141d1cdad957b6ebf699034d
JasonGITHUB <Jasonvirtualai@gmail.com>
2019-11-10 21:04:39 -0800

    Non

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.static('public'))
 
 app.use('/', indexRouter)
 
+
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 const db = mongoose.connection
